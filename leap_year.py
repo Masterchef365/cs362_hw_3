0e7316e4fde6bc60db105d262cab1c7b8b2571f5
Masterchef365 <duncan.freeman1@gmail.com>
2021-04-25 14:27:22 -0700

    User input without error handling

--- leap_year.py
+++ leap_year.py
@@ -7,7 +7,12 @@ def is_leap_year(y):
     return y % 400 == 0
 
 def main():
-    pass
+    year = int(input("Enter a year: "))
+    if is_leap_year(year):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+
 
 if __name__ == "__main__":
     main()
